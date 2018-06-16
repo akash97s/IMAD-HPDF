040d88bb0c1198655c1a176bd6d436b8ed3eef0b
Akash Selvakumar <akashselvae7@gmail.com>
2018-06-16 12:45:09 +0530

    Complete Modficition
    Twitter Timeline total modictaion commit

---
+++ Week1 - ReactJs/Main Timeline/src/Adhaar.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import './Adhaar.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import Avatar from 'material-ui/Avatar';
+import Badge from 'material-ui/Badge';
+import IconButton from 'material-ui/IconButton';
+import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import FontIcon from 'material-ui/FontIcon';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/IconMenu';
+import {
+  blue300,
+  indigo900,
+  orange200,
+  purple500,
+  white,
+} from 'material-ui/styles/colors';
+
+class Adhaar extends Component {
+
+  render()
+  {
+  const stylemain={ backgroundColor:white,height:45};
+  const style2={ marginRight:20,marginTop:-12};
+  const style3={ marginRight:20,marginTop:-12};
+  const style4={ backgroundColor:white,height:45,color:'#85929E' };
+  const style6={marginLeft:10};
+  const style1={marginTop:50,marginLeft:10};
+
+    return (
+
+      <MuiThemeProvider>
+      <div id="maindiv">
+      <div id="div1">
+      <AppBar
+        iconElementLeft={ <div id="Abh1"><h4>&emsp;&emsp;&emsp;&emsp;<img src="Twitterbird.jpg" id="icon1"/>&emsp;
+        <a href="www.twitter.com">Home</a>&emsp;&emsp;<a href="www.twitter.com">
+      About</a></h4> </div>}
+         iconElementRight={ <div id="Abh2"><div id="abh2"><input type="text" placeholder="Search Twitter" id="searchbox"/>
+      <button type="submit" id="b2"><span class="glyphicon glyphicon-search" id="searchicon"></span></button>Have an account?</div>
+      <DropDownMenu value={1} style={style2} >
+          <MenuItem value={1} primaryText="Log in" />
+          <MenuItem value={2} primaryText="Sign-up" />
+          <MenuItem value={3} primaryText="Guest" />
+        </DropDownMenu></div>}
+         style={stylemain} showMenuIconButton="false"  />
+<h3>aadhaar</h3>
+<div id="Ab2">
+<AppBar
+          iconElementLeft={<div id="Ab2h1"><h5>&emsp;&emsp;&emsp;&emsp;&emsp;
+          <a href="www.twitter.com">Top</a>&emsp;&emsp;<a href="www.twitter.com">
+        Latest</a>&emsp;&emsp;<a href="www.twitter.com">
+      People</a>&emsp;&emsp;<a href="www.twitter.com">
+    Photos</a>&emsp;&emsp;<a href="www.twitter.com">
+  Videos</a>&emsp;&emsp;<a href="www.twitter.com">
+News</a>&emsp;&emsp;<a href="www.twitter.com">
+Broadcast</a></h5></div> }
+iconElementRight={<IconButton><MoreVertIcon /></IconButton>} style={style4}/>
+</div>
+</div>
+<br/>
+<div id="maindiv2">
+<div id="div2">
+<div id="subdiv1">
+<h3>Related searches</h3><br/>
+<h4 class="trendsh1">Ochki</h4>
+<h4 class="trendsh1">Godrej & Boyce</h4>
+<h4 class="trendsh1">Rest in Peace</h4>
+<h4 class="trendsh1">Jayant savan</h4>
+<h4 class="trendsh1">Dhaval</h4>
+</div>
+<br/>
+<div id="subdiv2">
+<h3>Trends for you</h3><br/>
+<h4 class="trendsh1">#TheGrandTour </h4>
+<h5 class="trendsh2">12000 tweets</h5>
+<h4 class="trendsh1">#Thalapathy </h4>
+<h5 class="trendsh2">10000 tweets</h5>
+<h4 class="trendsh1">#Thala </h4>
+<h5 class="trendsh2">8000 tweets</h5>
+<h4 class="trendsh1">#MaxHospital </h4>
+<h5 class="trendsh2">2000 tweets</h5>
+<h4 class="trendsh1">#TheTour </h4>
+<h5 class="trendsh2">1000 tweets</h5>
+<h4 class="trendsh1">#Velaikkara </h4>
+<h5 class="trendsh2">100 tweets</h5>
+
+</div>
+</div>
+<div id="div3">
+<div id="subdiv3">
+<h4 id="sb3h1"><a href="Paypal.html">Top news</a></h4>
+<img src="Toi.jpg" height="350px" width="530px"/>
+<br/><br/>
+<h4 id="sb3h2"><a href="/">1000 emails in three hours against Aadhaar linking from new petition website - Times of India</a></h4>
+<h5 id="sb3h2">NEW DELHI: On the day the government extended the deadline to link Aadhar with bank accounts,
+ activists started a website on Wednesday called speakforme.</h5>
+ <div id="posticons"><Avatar
+        src="Toi.jpg"
+        size={40}
+        style={style1}/><h5>Dec13,2017</h5>
+</div>
+</div>
+</div>
+<div id="div4">
+<div id="subdiv4">
+<h5 id="copyrightsh1">© 2017 TwitterAboutHelp CenterTerms<br/><br/>
+Privacy policyCookiesAds infoBrandBlog<br/><br/>
+StatusAppsJobsMarketingBusinesses<br/><br/>
+Developers
+</h5>
+</div>
+</div>
+</div>
+      </div>
+            </MuiThemeProvider>
+      );
+      }
+    }
+    export default Adhaar;
--- Week1 - ReactJs/Main Timeline/src/App.js
+++ Week1 - ReactJs/Main Timeline/src/App.js
@@ -19,7 +19,7 @@ import Retweet from 'material-ui/svg-icons/av/repeat';
 import Moments from 'material-ui/svg-icons/image/flash-on';
 import MailO from 'material-ui/svg-icons/communication/mail-outline';
 import Chat from 'material-ui/svg-icons/communication/chat-bubble-outline';
-
+import IconButton from 'material-ui/IconButton';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import {
   blue300,
@@ -31,57 +31,108 @@ import {
   blue100,
   transparent
 } from 'material-ui/styles/colors';
-
+import {GridList, GridTile} from 'material-ui/GridList';
+import Chip from 'material-ui/Chip';
 
 class App extends Component {
 
   render() {
-    const style1 = { marginLeft:10};
-    const style2 = { marginTop:15,marginLeft:10};
+
+    function handleRequestDelete() {
+      alert('You clicked the delete button.');
+    }
+
+    function handleClick() {
+      alert('You clicked the Chip.');
+    }
+
+    const style1 = { marginLeft:10,marginTop:-2};
+    const style2 = { marginTop:12,marginLeft:10};
     const stylemain={ backgroundColor:white,height:45};
     const style3={ width:25,height:30};
     const style4={ width:600,height:80,backgroundColor:'#D5F5E3'};
     const style5= { marginTop:-28,marginLeft:10,border:25,borderColor:white};
     const style6={ height:320,width:500,marginLeft:70};
-    const style7={ height:65,backgroundColor:'#B2DFDB'};
-    const iconStyles= { height:30,width:25};
+    const style7={ height:72,backgroundColor:'#E8F5FD'};
+    const iconStyles= { height:30,width:25,marginTop:5};
     const icon1= { height:25,width:20};
+    const style8= {marginLeft:5};
+    const style9= {marginTop:-15};
 
+    const styles = {
+      root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+      },
+      gridList: {
+        width: 550,
+        height: 375,
+        overflowY: 'auto',
+      },
+    };
+    const tilesData = [
+  {
+    img: 'c1.jpg',
+    title: 'Breakfast',
+    author: 'jill111',
+    featured : true,
+  },
+  {
+    img: 'c2.jpeg',
+    title: 'Tasty burger',
+    author: 'pashminu',
+  },
+  {
+    img: 'c3.jpg',
+    title: 'Camera',
+    author: 'Danson67',
+  },
+  {
+    img: 'c4.jpg',
+    title: 'Morning',
+    author: 'fancycrave1',
+    featured : true,
+  }
+];
 return (
 
 <MuiThemeProvider>
+
  <div id="div1">
+
  <AppBar
-   iconElementLeft={ <div id="Abheader1"><h5>&emsp;&emsp;&emsp;<a href="/"><Home color={black} style={iconStyles}/>
+   iconElementLeft={ <div id="Abheader1"><h5>&emsp;&emsp;&emsp;<a href="/"><Home color={black} hoverColor={blue300} style={iconStyles}/>
   Home</a>&emsp;<a href="/"><Moments style={iconStyles} color={black} hoverColor={blue300} />Moments</a>
   &emsp;&emsp;<a href="/"><Notifications style={iconStyles} color={black} hoverColor={blue300} />Notifications</a>
   &emsp;&emsp;<a href="/"><Mail style={iconStyles} color={black} hoverColor={blue300} />Messages</a></h5>
   <img src="Twitterbird.jpg" height="25px" width="30px" id="Tbird"/></div> }
-  iconElementRight={ <div id="Abheader2"><input type="text" placeholder="Search Twitter" id="searchbox"/>
-  <a href="adhaar.html"><button type="submit" id="b2"><img src="s2.png" height="15px" width="15px" id="sicon"/></button>
-  </a>&emsp;&emsp;
+  iconElementRight={ <div id="Abheader2"><input type="text" placeholder="   Search Twitter" id="searchbox"/>
+  <a href='adhaar.html'>
+  </a> &emsp;&emsp;
    <Avatar
    src="pic1.jpg"
    size={32}
    style={style1}/>&emsp;<button type="submit" id="b1"><b>Tweet</b></button></div> }
    style={stylemain} showMenuIconButton="false" />
+
      <br/>
   <div id="maindiv1">
   <div id="div2">
   <div id="div2cp">
-  <h5>Cover Pic</h5>
+  <img src="cover.jpg" height="100%" width="100%"/>
   </div>
   <Avatar
   src="pic1.jpg"
   size={50}
   style={style5}/>
-        <center><h3>&emsp;&emsp;Akash Selvakumar</h3><h4 id="profh1">@Akash__Selva</h4></center><br/>
+        <center><h3>&emsp;&emsp;Akash Selvakumar</h3><h4 id="profh1">@Akash__Selva</h4></center>
         <h5 id="profh2">&emsp;&emsp;&emsp;Tweets&emsp;&emsp;Following&emsp;&emsp;Followers</h5>
-        <h5 id="profh3">&emsp;&emsp;&emsp;&emsp;0&emsp;&emsp;&emsp;&emsp;&emsp;15&emsp;&emsp;&emsp;&emsp;&emsp;10</h5>
+        <h5 id="profh3">&emsp;&emsp;0&emsp;&emsp;&emsp;&emsp;&emsp;15&emsp;&emsp;&emsp;&emsp;&emsp;10</h5>
       </div>
       <br/>
       <div id="div3">
-      <h2>Trends for you</h2>
+      <h3>Trends for you</h3>
       <h4 class="trendsh1">#TheGrandTour </h4>
       <h5 class="trendsh2">12000 tweets</h5>
       <h4 class="trendsh1">#Thalapathy </h4>
@@ -101,91 +152,132 @@ return (
 <div id="subdiv4">
 <AppBar iconElementLeft={<Avatar
       src="pic1.jpg"
-      size={35}
+      size={40}
       style={style2}/>} iconElementRight={<div id="divWh"><input type="text" id="text1" placeholder=" What's happenings?"/></div>}
 style={style7} showMenuIconButton="false" />
 </div>
-<h2 id="div4h1">Chennaissss Super Kings <a href="/">@CSKOFFICIAL</a></h2>
-<h4 id="div4h">The kings are back</h4><p id="div4p">&emsp; #WhistlePodu #BleedYellow #RoaroftheLions #Chepauk</p><br/>
+<div id="m2div1">
+<h3 id="div4h1"><Avatar
+   src="p2.jpg"
+   size={50}
+   style={style8}/>&emsp;Chenn Super Kings &emsp;<a href="/" class="d4s">@CSKOFFICIAL</a></h3>
+<h4 id="div4h">&emsp;&emsp;&emsp;CSK is back for IPL 2018<br/>
+&emsp;&emsp;&emsp;Retained: M S Dhoni , Suresh Raina and Ravidra Jadeja</h4>
+<p id="div4p">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;#WhistlePodu #BleedYellow #RoaroftheLions #Chepauk</p>
 <Card style={style6}>
-<center><img src="2.jpg" height="250px" width="500px"/>
-<CardText>
-      <b>CSK is back for IPL 2018 <br/>Retained: M S Dhoni , Suresh Raina and Ravidra Jadeja</b>
-    </CardText>
+<center><img src="2.jpg" height="319px" width="500px"/>
 </center>
 </Card>
 <br/>
-<div id="div4icons">
-<Chat style={iconStyles} color={black} hoverColor={blue300} />&emsp;100&emsp;&emsp;&emsp;
-<Retweet style={iconStyles} color={black} hoverColor={blue300} />&emsp;200&emsp;&emsp;&emsp;
-<Love style={iconStyles} color={black} hoverColor={blue300} />&emsp;300&emsp;&emsp;&emsp;
+<div class="div4icons">
+<IconButton tooltip="Reply" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Chat style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>100 &emsp;
+<IconButton tooltip="Retweet" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Retweet style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>200&emsp;
+<IconButton tooltip="Like" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Love style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>300&emsp;
+<IconButton tooltip="Direct Message" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
 <MailO style={iconStyles} color={black} hoverColor={blue300} />
+    </IconButton>
       </div>
-      </div>
-      <br/>
+        </div>
+         </div>
 <div id="md2div2">
-<h2>&emsp;Kolly Buzz &emsp;<a href="/">@KollyBuzzOfficial</a></h2>
-<h4>&emsp;&emsp;&emsp;Storms into 250cr club</h4>
+<h3><Avatar
+   src="p3.jpg"
+   size={50}
+   style={style8}/>&emsp;Kolly Buzz &emsp;<a href="/" class="d4s">@KollyBuzzOfficial</a></h3>
+<h4>&emsp;&emsp;&emsp;&emsp;&emsp;Mersal a blockbuster of 2k18<br/>
+&emsp;&emsp;&emsp;&emsp;&emsp;Mersal enters the elite 250cr club</h4>
 <Card style={style6}>
-<center><img src="3.jpg" height="250px" width="500px"/>
-<CardText>
-      <b>Mersal a blockbuster of 2k18<br/> Mersal enters the elite 250cr club</b>
-    </CardText>
+<center><img src="3.jpg" height="319px" width="500px"/>
 </center>
 </Card>
 <br/>
-<div id="div4icons1">
-<Chat style={iconStyles} color={black} hoverColor={blue300} />&emsp;100&emsp;&emsp;&emsp;
-<Retweet style={iconStyles} color={black} hoverColor={blue300} />&emsp;200&emsp;&emsp;&emsp;
-<Love style={iconStyles} color={black} hoverColor={blue300} />&emsp;300&emsp;&emsp;&emsp;
+<div class="div4icons">
+<IconButton tooltip="Reply" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Chat style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>100 &emsp;
+<IconButton tooltip="Retweet" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Retweet style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>200&emsp;
+<IconButton tooltip="Like" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Love style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>300&emsp;
+<IconButton tooltip="Direct Message" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
 <MailO style={iconStyles} color={black} hoverColor={blue300} />
+    </IconButton>
     </div>
     </div>
-    <br/>
 <div id="md2div3">
-<h2>&emsp;Chennaiyin Fc &emsp;<a href="/">@ChennayinFc</a></h2>
-<h4>&emsp;&emsp;&emsp;Chennai face Mumbai </h4>
+<h3><Avatar
+   src="p1.jpg"
+   size={50}
+   style={style8}/>&emsp;Chennaiyin Fc &emsp;<a href="/" class="d4s">@ChennayinFc</a></h3>
+<h4>&emsp;&emsp;&emsp;&emsp;&emsp;Chennayin face another tough challenge<br/>
+&emsp;&emsp;&emsp;&emsp;&emsp;Dont miss the epic clash tonight</h4>
 <Card style={style6}>
-<center><img src="4.jpg" height="250px" width="500px"/>
-<CardText>
-      <b>Chennayin face anothr tough challenge<br/>Dont miss the epic clash tonight</b>
-    </CardText>
+<center><img src="4.jpg" height="319px" width="500px"/>
 </center>
 </Card>
 <br/>
-<div id="div4icons2">
-<Chat style={iconStyles} color={black} hoverColor={blue300} />&emsp;100&emsp;&emsp;&emsp;
-<Retweet style={iconStyles} color={black} hoverColor={blue300} />&emsp;200&emsp;&emsp;&emsp;
-<Love style={iconStyles} color={black} hoverColor={blue300} />&emsp;300&emsp;&emsp;&emsp;
+<div class="div4icons">
+<IconButton tooltip="Reply" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Chat style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>100 &emsp;
+<IconButton tooltip="Retweet" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Retweet style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>200&emsp;
+<IconButton tooltip="Like" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
+<Love style={iconStyles} color={black} hoverColor={blue300} />
+</IconButton>300&emsp;
+<IconButton tooltip="Direct Message" touch={true} tooltipPosition="top-center" hoveredStyle={style9} >
 <MailO style={iconStyles} color={black} hoverColor={blue300} />
+    </IconButton>
       </div>
       </div>
       </div>
       <div id="maindiv3">
       <div id="div5">
- <h3>Who to follow</h3><a href="/">Refresh</a>&emsp;<a href="/">View all</a>
- <h4>Suriya Sivakumar @SuriyaOffi</h4>
- <button class="suggestionsb1"><b>Follow</b></button>
+ <h3>Who to follow</h3><a href="/" class="d5hd">Refresh</a>&emsp;<a href="/" class="d5hd">View all</a>
+          <div id="d5">
+        <Avatar
+    src="s.jpg"
+    size={50}
+    style={style9}/><div id="d5h"><h5>&emsp;Suriya Sivakumar <a href="/"  class="d5a">@SuriyaOffi</a></h5>
+ <button class="suggestionsb1"><b>Follow</b></button></div>
+ </div>
 <hr/>
- <h4>Vijay @ThalapathyaOffi</h4>
-<button class="suggestionsb1"><b>Follow</b></button>
+<div id="d51"><Avatar
+    src="v.jpg"
+    size={50}
+    style={style9}/><div id="d5h1"><h5>&emsp;Vijay <a href="/"  class="d5a">@ThalapathyaOffi</a></h5>
+<button class="suggestionsb1"><b>Follow</b></button></div>
+</div>
 <hr/>
- <h4>Ajith Kumar @ThalaaOffi</h4>
- <button class="suggestionsb1"><b>Follow</b></button>
+<div id="d52"><Avatar
+    src="a.jpg"
+    size={50}
+    style={style9}/><div id="d5h2"><h5>&emsp;Ajith Kumar <a href="/" class="d5a">@ThalaaOffi</a></h5>
+ <button class="suggestionsb1"><b>Follow</b></button></div>
+ </div>
       </div>
       <br/>
       <div id="div6">
-<h5 id="copyrightsh1">© 2017 TwitterAboutHelp CenterTerms<br/><br/>
-Privacy policyCookiesAds infoBrandBlog<br/><br/>
-StatusAppsJobsMarketingBusinesses<br/><br/>
+<h5 id="copyrightsh1">© 2017 Twitter About Help Center Terms<br/><br/>
+Privacy policy Cookies Ads info Brand Blog<br/><br/>
+Status Apps Jobs Marketing Businesses<br/><br/>
 Developers
 </h5>
-
+      </div>
+      </div>
       </div>
 
-      </div>
-      </div>
         </MuiThemeProvider>
+
     );
   }
 }
